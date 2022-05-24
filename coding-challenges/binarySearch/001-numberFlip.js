15b65b558e6a6366c782f1204d8206435e5a5f2f
ardevsrc <dbeast@tuta.io>
2022-05-24 01:28:31 -0400

    Added 001-numberFlip

---
+++ coding-challenges/binarySearch/001-numberFlip.js
@@ -0,0 +1,18 @@
+/* 123 Number Flip:
+  You are given an integer n consisting of digits 1, 2, and 3 and you can flip one digit to a 3. 
+  Return the maximum number you can make.
+
+  Constraints
+  1 ≤ n < 2 ** 31
+
+  Example 1:
+  Input
+  n = 123
+
+  Output
+  323
+
+  Explanation
+  We flip 1 to 3
+
+*/
