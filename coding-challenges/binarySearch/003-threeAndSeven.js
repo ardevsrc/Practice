d62d76bfe2bb3a44d307b65dac86e2fe90214bae
ardevsrc <dbeast@tuta.io>
2022-05-25 23:47:16 -0400

    Added 003-threeAndSeven

---
+++ coding-challenges/binarySearch/003-threeAndSeven.js
@@ -0,0 +1,25 @@
+/* 3 and 7
+  Given a positive integer n, determine whether you can make n 
+  by summing up some non-negative multiple of 3 and some non-negative multiple of 7.
+
+  Constraints
+  n < 2 ** 31
+
+  Example 1:
+  Input
+  n = 13
+
+  Output
+  True
+
+  Explanation
+  We can get 13 with 1 * 7 + 2 * 3.
+
+  Example 2:
+  Input
+  n = 9
+
+  Output
+  True
+
+*/
